afc881f17cc7a4dfe1006f595bc0023bb034a56f
bobbybwoy <68812513+bobbybwoy@users.noreply.github.com>
2026-03-31 18:55:56 +0100

    Completed section "Product Images Component"

--- app/(root)/product/[slug]/page.tsx
+++ app/(root)/product/[slug]/page.tsx
@@ -4,6 +4,7 @@ import { Card, CardContent } from "@/components/ui/card";
 import ProductPrice from "@/components/shared/product/product-price";
 import { getProductBySlug } from "@/lib/actions/product.actions";
 import { notFound } from "next/navigation";
+import ProductImages from "@/components/shared/product/product-images";
 
 const ProductDetailsPage = async (props: { params: Promise<{ slug: string }> }) => {
     const { slug } = await props.params;
@@ -18,6 +19,7 @@ const ProductDetailsPage = async (props: { params: Promise<{ slug: string }> }) 
                     {/* Images Column */}
                     <div className="col-span-2">
                         {/* Images Component */}
+                        <ProductImages images={product.images} />
                     </div>
 
                     {/* Details Column */}
